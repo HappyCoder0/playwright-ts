820126dda9cb6216d8de67f77507817e9226eb1b
HappyCoder0 <technosoura@gmail.com>
2025-04-01 14:09:46 +0200

    first commit

---
+++ ReadMe.md
@@ -0,0 +1,26 @@
+## Installation
+Prerequisites:
+NodeJS 14(or above) and Java 8 (or above)
+
+Navigate to project folder. Open Terminal.
+Install Playwright:
+```bash
+npm init playwright@latest
+```
+## Run Tests in default mode (headless)
+```bash
+npx playwright test
+```
+## Run Tests in UI mode
+```bash
+npx playwright test --ui
+```
+## Run Tests in headed mode
+```bash
+npx playwright test --headed
+```
+
+## Show Reports
+```bash
+npx playwright show-report
+```
---
+++ tests/osapiens-test.spec.ts
@@ -0,0 +1,33 @@
+import { test, expect } from '@playwright/test';
+
+/** Improvement idea: The below test can be improved by:
+ * Page-Object model for re-usability of locators, methods
+ * Storing test data in a Json and passing them in tests
+ * */
+
+test('should have at least 1 job title with `Quality`', async ({ page }) => {
+    let checkTitleExists = false;
+    // Navigate to URL
+    await page.goto('https://osapiens.jobs.personio.com/');
+
+    // Print number of open jobs
+    const jobCount = await page.locator('[class=\'job-summary-desc-info-jobs\']').textContent();
+    console.log("Total number of open jobs is " + jobCount);
+
+    // Check if expected job Title exists
+    const jobTitleLocator = await page.locator("div[id='all']>section>div>a>div>div[class='jb-title']");
+    const jobTitlesElementsList = await jobTitleLocator.all();
+    for(const row of jobTitlesElementsList) {
+        const jobTitle = await row.textContent();
+        checkTitleExists = jobTitle.includes("Quality");
+        if(checkTitleExists == true) {
+            break;
+        }
+    }
+
+    /** Improvement idea: The above code can be optimised using Fluent/chain approach as below*/
+    // checkTitleExists = await jobTitleLocator.locator("text=Quality").count() > 0;
+
+    expect(checkTitleExists).toBe(true);
+
+});
